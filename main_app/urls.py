c53b12b0c84690931de3680b2ef95d5ae74c65f4
Brian Krabec <briankrabec@gmail.com>
2023-01-26 13:14:55 -0600

    Models walkthrough completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('about/', views.About.as_view(), name="about"),
     path('artists/', views.ArtistList.as_view(), name="artist_list"),
     path('songs/', views.SongList.as_view(), name="song_list"),
+    # Creating an Artist
+    path('artists/new/', views.ArtistCreate.as_view(), name="artist_create"),
+    # Show urlpattern
+    path('artists/<int:pk>', views.ArtistDetail.as_view(), name="artist_detail"),
+    # Update urlpattern
+    path('artists/<int:pk>/update', views.ArtistUpdate.as_view(), name="artist_update"),
+    # Delete urlpattern
+    path('artists/<int:pk>/delete', views.ArtistDelete.as_view(), name="artist_delete")
 ]
